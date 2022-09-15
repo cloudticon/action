124d87c076039202fc81e653f015a35e5f19424a
krs <k.kamasa@gmail.com>
2022-09-15 09:54:36 +0200

    init

--- src/index.ts
+++ src/index.ts
@@ -10,10 +10,12 @@ export * from "./utils/getRepositoryOutput";
 
 import * as core from "@actions/core";
 import { compileCt } from "./utils/compileCt";
+import * as fs from "fs";
 
 async function run(): Promise<void> {
   try {
     await compileCt();
+    console.log(fs.readFileSync(context.workingDir));
     core.setOutput("time", new Date().toTimeString());
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message);
