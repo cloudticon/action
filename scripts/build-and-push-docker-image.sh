8dc264d166d744c17b67afb1e92d571e83c40a5d
krs <k.kamasa@gmail.com>
2022-09-15 17:24:08 +0200

    init

--- scripts/build-and-push-docker-image.sh
+++ scripts/build-and-push-docker-image.sh
@@ -3,5 +3,5 @@ set -e;
 CTX=$1
 IMAGE=$2
 
-docker buildx build $CTX -t $IMAGE --cache-from type=local,src=/opt/docker-cache --cache-to type=local,src=/opt/docker-cache
+docker buildx build $CTX -t $IMAGE --cache-from type=local,src=/opt/docker-cache --cache-to type=local,dest=/opt/docker-cache
 docker push $IMAGE
--- src/utils/setupBuildx.ts
+++ src/utils/setupBuildx.ts
@@ -1,6 +1,7 @@
 import * as tc from "@actions/tool-cache";
 import * as core from "@actions/core";
 import * as fs from "fs";
+import { exec } from "@actions/exec";
 
 export const setupBuildx = async () => {
   core.info("Download docker buildx");
@@ -9,4 +10,5 @@ export const setupBuildx = async () => {
   const binaryPath = await tc.downloadTool(url, "/tmp/docker-buildx/buildx");
   core.addPath("/tmp/docker-buildx");
   fs.chmodSync(binaryPath, "777");
+  await exec("docker buildx create --use");
 };
