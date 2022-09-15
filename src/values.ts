36a62523d1b0667e65078b91028f7d2ba8a58876
krs <k.kamasa@gmail.com>
2022-09-15 09:36:05 +0200

    init

--- src/values.ts
+++ src/values.ts
@@ -1,11 +1,7 @@
 import { tfg } from "./tfg";
 import { Variable } from "terraform-generator";
-import * as fs from "fs";
-import { context } from "./context";
 
-console.log(context);
-const allValues = JSON.parse(fs.readFileSync("values.json", "utf-8"));
-const values = allValues[context.repository][context.branch];
+const values = {};
 
 export const variables: Record<string, Variable> = {};
 Object.entries(values).forEach(([name, value]) => {
