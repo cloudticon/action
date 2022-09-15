e678e498f299c4e6b08a4b346e237fd413f53712
krs <k.kamasa@gmail.com>
2022-09-15 17:42:50 +0200

    init

--- scripts/build-and-push-docker-image.sh
+++ scripts/build-and-push-docker-image.sh
@@ -3,5 +3,4 @@ set -e;
 CTX=$1
 IMAGE=$2
 
-docker buildx build $CTX -t $IMAGE --cache-from type=local,src=/tmp/docker-cache --cache-to type=local,dest=/tmp/docker-cache-new
-docker push $IMAGE
+docker buildx build $CTX -t $IMAGE --cache-from type=local,src=/tmp/docker-cache --cache-to type=local,dest=/tmp/docker-cache-new --push
