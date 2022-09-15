ade3df50041a055b5d905bca8a05128835ca7bc6
krs <k.kamasa@gmail.com>
2022-09-15 11:48:58 +0200

    init

--- dist/swc.js
+++ dist/swc.js
@@ -1,5 +1,12 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 42:
+/***/ ((__unused_webpack_module, exports) => {
+
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
 exports.createSwcOptions = exports.targetMapping = exports.create = void 0;
 function create(createOptions) {
     const { swc, service: { config, projectLocalResolveHelper }, transpilerConfigLocalResolveHelper, nodeModuleEmitKind, } = createOptions;
@@ -210,3 +217,23 @@ function createSwcOptions(compilerOptions, nodeModuleEmitKind, swcInstance, swcD
 }
 exports.createSwcOptions = createSwcOptions;
 //# sourceMappingURL=swc.js.map
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module is referenced by other modules so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__[42](0, __webpack_exports__);
+/******/ 	module.exports = __webpack_exports__;
+/******/ 	
+/******/ })()
+;
---
+++ test/test.js
@@ -0,0 +1,4 @@
+process.env.GITHUB_REPOSITORY = "payticon-3/backend";
+process.env.GITHUB_REF = "test/test/development";
+process.env.GITHUB_WORKSPACE = "/home/krs/Projects/payticon/backend";
+require("./dist/index.js");
