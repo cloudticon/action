f13dd8613d94c2977949b65075d2406b20057a1b
krs <k.kamasa@gmail.com>
2022-09-15 15:09:36 +0200

    up

--- dist/hasura-deploy.sh
+++ dist/hasura-deploy.sh
@@ -1,4 +1,4 @@
 set -e;
 
-hasura --skip-update-check migrate apply --insecure-skip-tls-verify
+hasura --skip-update-check migrate apply --insecure-skip-tls-verify --all-databases
 hasura --skip-update-check metadata apply --insecure-skip-tls-verify
