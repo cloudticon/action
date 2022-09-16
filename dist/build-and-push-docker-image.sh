dfc1dbe0347090226b65e850846d60f371564fce
krs <k.kamasa@gmail.com>
2022-09-16 14:38:08 +0200

    up

--- dist/build-and-push-docker-image.sh
+++ dist/build-and-push-docker-image.sh
@@ -11,7 +11,7 @@ docker build $CTX \
     -t $IMAGE \
     --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 \
     --cache-from type=local,src=/tmp/docker-cache \
-    --cache-to type=local,dest=/tmp/docker-cache-new \
+    --cache-to type=local,mode=max,dest=/tmp/docker-cache-new \
     --push
 
 rm -rf /tmp/docker-cache
