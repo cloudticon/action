3f86a51f570637b1685acadfaacec38781ea5dbf
krs <k.kamasa@gmail.com>
2022-09-16 14:07:02 +0200

    up

--- dist/build-and-push-docker-image.sh
+++ dist/build-and-push-docker-image.sh
@@ -4,7 +4,7 @@ set -e;
 CTX=$1
 IMAGE=$2
 
-docker buildx build $CTX \
+docker build $CTX \
     -t $IMAGE \
     --cache-from type=local,src=/tmp/docker-cache \
     --cache-to type=local,dest=/tmp/docker-cache-new \
