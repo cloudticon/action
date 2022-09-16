1604f466450074ac403add10ee8e351ccc067c51
krs <k.kamasa@gmail.com>
2022-09-16 15:19:07 +0200

    up

--- dist/build-and-push-docker-image.sh
+++ dist/build-and-push-docker-image.sh
@@ -3,9 +3,10 @@ set -e;
 
 CTX=$1
 IMAGE=$2
+BUILDX_NO_DEFAULT_LOAD=1
 
 ls -la /tmp/docker-cache/blobs/sha256
-git --no-pager  diff --name-only
+
 docker build $CTX \
     -t $IMAGE \
     --build-arg BUILDKIT_CONTEXT_KEEP_GIT_DIR=1 \
