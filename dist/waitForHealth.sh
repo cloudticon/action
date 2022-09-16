89925e8aa3e42cb8d758d53de9c4eaf3caa993f6
krs <k.kamasa@gmail.com>
2022-09-16 10:55:21 +0200

    up

--- dist/waitForHealth.sh
+++ dist/waitForHealth.sh
@@ -1,3 +1,3 @@
 set -e;
-echo "Waiting for $URL $CODE"
-timeout 10 bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $URL)" != "$CODE" ]]; do sleep 5; done' || false
+echo "Waiting for $URL $CODE time: $TIME"
+timeout $TIME bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $URL)" != "$CODE" ]]; do sleep 5; done' || false
