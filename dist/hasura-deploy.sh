aeba3b0542fceb42cac817fb38975a146cd6d7da
krs <k.kamasa@gmail.com>
2022-10-12 10:13:18 +0200

    v1.1.15

--- dist/hasura-deploy.sh
+++
@@ -1,4 +0,0 @@
-set -x;
-
-hasura --skip-update-check migrate apply --insecure-skip-tls-verify --all-databases
-hasura --skip-update-check metadata apply --insecure-skip-tls-verify
--- dist/waitForHealth.sh
+++
@@ -1,3 +0,0 @@
-set -e;
-echo "Waiting for $URL $CODE time: $TIME"
-timeout $TIME bash -c 'while [[ "$(curl -k -s -o /dev/null -w ''%{http_code}'' $URL)" != "$CODE" ]]; do sleep 5; done' || false
