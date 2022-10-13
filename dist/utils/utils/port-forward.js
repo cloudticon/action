980236eec520d31710c76d3a691875cb626e33a3
krs <k.kamasa@gmail.com>
2022-10-13 11:30:28 +0200

    up

---
+++ dist/utils/utils/port-forward.js
@@ -0,0 +1,26 @@
+const { spawn } = require("child_process");
+
+module.exports = function portForward({
+  deployment,
+  portForm,
+  portTo,
+  namespace,
+}) {
+  return new Promise((resolve, reject) => {
+    const subProcess = spawn(
+      "kubectl",
+      ["port-forward", deployment, `${portForm}:${portTo}`, "-n", namespace],
+      {}
+    );
+    subProcess.stdout.on("data", (data) => {
+      if (data.toString().includes("Forwarding from")) {
+        console.log("port forwarded", data.toString());
+        resolve(() => subProcess.kill());
+      }
+    });
+
+    subProcess.stderr.on("data", (data) => {
+      reject(new Error(data.toString()));
+    });
+  });
+};
