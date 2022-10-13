6a8dd21cb0d1cfc19a6bca305cd8b67904062fef
krs <k.kamasa@gmail.com>
2022-10-13 11:24:35 +0200

    up

---
+++ dist/prisma-deploy.js
@@ -0,0 +1,17 @@
+const prismaDeploy = require(__dirname + "/utils/port-forward");
+const exec = require(__dirname + "/utils/exec");
+
+async function main() {
+  const closePortForward = await prismaDeploy({
+    deployment: process.env.DEPLOYMENT,
+    portFrom: process.env.PORT_FROM,
+    portTo: process.env.PORT_TO,
+    namespace: process.env.NAMESPACE,
+  });
+  await exec("yarn", ["prisma", "migrate", "deploy"], {
+    cwd: "/home/krs/Projects/payticon/frontend-eshop",
+    env: process.env,
+  });
+  closePortForward();
+}
+main();
